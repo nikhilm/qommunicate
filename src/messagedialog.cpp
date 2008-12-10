5406971f8da12999c10da7a6c7dcccca7c10ef0a
Nikhil Marathe <nsm.nikhil@gmail.com>
2008-12-10 11:45:16 +0530

    Changed title string to better reflect job of the dialog

--- src/messagedialog.cpp
+++ src/messagedialog.cpp
@@ -1,13 +1,16 @@
 #include "messagedialog.h"
 
 #include "qommunicate.h"
+#include "messenger.h"
+#include "constants.h"
 
 MessageDialog::MessageDialog(Member* receiver, QWidget *parent) : QDialog(parent)
 {
     receivers << receiver;
     
     ui.setupUi(this);
-    setWindowTitle(tr("Send Message to %1").arg(receiver->name()));
+    setWindowTitle(tr("Conversation: %1").arg(receiver->name()));
+    connect(messenger(), SIGNAL(msg_recvMsg(Message)), this, SLOT(incomingMessage(Message)));
     
     ((Qommunicate*)parent)->dialogOpened(receiver);
     
@@ -23,7 +26,7 @@ MessageDialog::MessageDialog(QList<Member*> receivers, QWidget *parent) : QDialo
     Member* t;
     foreach(t, receivers)
         titleRecvs << t->name();
-    setWindowTitle(tr("Send message to %1").arg(titleRecvs.join(",")));
+    setWindowTitle(tr("Conversation: %1").arg(titleRecvs.join(",")));
 }
 
 MessageDialog::MessageDialog(QWidget *parent) : QDialog(parent)
@@ -37,4 +40,10 @@ void MessageDialog::closeEvent(QCloseEvent *evt)
     if(receivers.size() == 1)
         ((Qommunicate*) parent())->dialogClosed(receivers[0]);
     evt->accept();
+}
+
+void MessageDialog::incomingMessage(Message msg)
+{
+    ui.messageEdit->append(msg.payload());
+    messenger()->sendMessage(QOM_RECVMSG, QString::number(msg.packetNo()), receivers[0]);
 }
